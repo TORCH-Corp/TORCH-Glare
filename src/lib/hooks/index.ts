8f63ed91542e88f504157db18301a55d86d9f004
Sajjad-iq <rasooool59@gmail.com>
2024-05-26 11:38:11 +0300

    make index file for the hooks and export them to use in the lib main file

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,11 @@
+import UseDirectionCalc from "./useDirectionCalc";
+import { useDragEnd } from "./useDragEnd";
+import { useResize } from "./useResize";
+import { useShowDropDown } from "./useShowDropDown";
+
+export {
+    UseDirectionCalc,
+    useDragEnd,
+    useResize,
+    useShowDropDown,
+}
--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,6 +1,8 @@
 import { Label, Button, TopFormBarItem, SideFormBarItem, Badge, DropDownMenuItem, DropDownMenu, Datepicker, Tooltip, InputField, LabelLessInput, ActionBarInputField, NoteInputField, ButtonField, CellSizingLine, ItemPic, TableLabel, TableSort, CheckboxLabel, TableHeaderCell, TableIconCell, TableCell, Table, TableCellContainer, LinkButton, Alert, DropDownButton, DropDownButtonResultShow } from "./components/base/main";
+import { UseDirectionCalc, useDragEnd, useResize, useShowDropDown } from "./hooks";
 
 export {
+    /* components */
     Label,
     Button,
     TopFormBarItem,
@@ -28,5 +30,13 @@ export {
     LinkButton,
     DropDownButton,
     DropDownButtonResultShow,
-    Alert
+    Alert,
+
+
+
+    /* hooks */
+    UseDirectionCalc,
+    useDragEnd,
+    useResize,
+    useShowDropDown,
 }
