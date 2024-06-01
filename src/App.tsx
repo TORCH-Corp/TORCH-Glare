e1fc2373b728f0179d2d2bee2ca1c2f8caf923c4
Sajjad-iq <rasooool59@gmail.com>
2024-06-01 11:33:39 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { InputField } from "../dist"
 
 function App() {
 
   return (
     <section className='app' >
-      <InputField className="test" label="Label" name={""} theme='System-Style' component_size='L' error_message="Email is Required" />
     </section >
   )
 }
