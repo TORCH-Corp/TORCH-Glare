aa3ab7bac06d7b012401bda5bf77823e215188eb
Sajjad-iq <rasooool59@gmail.com>
2024-06-22 12:32:10 +0300

    import the ContentColumn & FieldSection to the entry point file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { Label, Button, TabFormItem, Badge, DropDownMenuItem, DropDownMenu, Datepicker, Tooltip, InputField, LabelLessInput, ActionBarInputField, NoteInputField, ButtonField, CellSizingLine, ItemPic, TableLabel, TableSort, CheckboxLabel, TableHeaderCell, TableIconCell, TableCell, Table, TableCellContainer, LinkButton, Alert, DropDownButton, DropDownButtonResultShow, RadioLabel, RingLoading, Counter } from "./components/base";
-import { ProfileItem, AuthButton, PasswordLevel, SideBarItem, SmallButton, FeedBackItem } from "./components/shared";
+import { ProfileItem, AuthButton, PasswordLevel, SideBarItem, SmallButton, FeedBackItem, ContentColumn, FieldSection } from "./components/shared";
 import { UseDirectionCalc, useDragEnd, useResize, useShowDropDown } from "./hooks";
 
 export {
@@ -45,6 +45,8 @@ export {
     PasswordLevel,
     SmallButton,
     FeedBackItem,
+    ContentColumn,
+    FieldSection,
 
 
 
