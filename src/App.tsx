b0ad69359d708579b8c40963d6d2e56716fa5926
Sajjad-iq <rasooool59@gmail.com>
2024-06-25 13:37:42 +0300

    authButton & Inputs outline color fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
 import 'torch-glare/dist/themes/colorMapping/dark.css';
-import { Badge, DropDownMenu, FieldSection } from './lib';
+import { AuthButton } from './lib';
 
 function App() {
 
   return (
     <section className='app' >
-      <FieldSection
-        component_size="M"
-        secondary_label=""
-        label="Modules"
-        required_label="Required"
-        name={"modules"}
-        readOnly
-
-        badges_children={
-          <>
-            <Badge
-              badge_style='badge-blue'
-              badge_size='M'
-              label={'e.namedddddddddddddddddddddddddd'}
-              selected />
-          </>
-        }
-
-        drop_down_list_child={
-          <DropDownMenu component_style='System-Style'>
-            <Badge
-              badge_style='badge-blue'
-              badge_size='M'
-              label={'e.namdddddddddddddddddddddddde'}
-            />
-
-          </DropDownMenu>
-        }
-      />
+      <AuthButton>Label</AuthButton>
     </section >
   )
 }
