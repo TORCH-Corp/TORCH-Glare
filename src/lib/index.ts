3504402206bd41987bfaa925dd1af52c97b835f5
Sajjad-iq <rasooool59@gmail.com>
2024-07-25 13:16:10 +0300

    add docs for the hooks

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 import { Label, Button, TabFormItem, Badge, DropDownMenuItem, DropDownMenu, Datepicker, Tooltip, InputField, LabelLessInput, ActionBarInputField, NoteInputField, ButtonField, CellSizingLine, ItemPic, TableLabel, TableSort, CheckboxLabel, TableHeaderCell, TableIconCell, TableCell, Table, TableCellContainer, LinkButton, Alert, DropDownButton, DropDownButtonResultShow, RadioLabel, RingLoading, Counter, StepperTab, StepperPhoneArrow, StepperDivider } from "./components/base";
+import { DynamicContainer } from "./components/helpers";
 import { ProfileItem, LoginButton, PassCheck, SideBarItem, IconButton, FeedBackItem, ContentColumn, FieldSection } from "./components/shared";
 import { UseDirectionCalc, useDragEnd, useResize, useShowDropDown } from "./hooks";
 import 'remixicon/fonts/remixicon.css'
@@ -41,6 +42,8 @@ export {
     StepperDivider,
 
 
+    /* Helpers Components */
+    DynamicContainer,
 
     /* shared components */
     ProfileItem,
---
+++ src/stories/helpers/DynamicContainer.stories.tsx
@@ -0,0 +1,71 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { ComponentProps } from 'react';
+import { DropDownMenu, DropDownMenuItem, DynamicContainer } from '../../lib'
+
+
+
+type StoryProps = ComponentProps<typeof DynamicContainer> & {
+};
+
+const meta: Meta<StoryProps> = {
+    component: DynamicContainer,
+    title: 'Helpers/DynamicContainer',
+    argTypes: {
+        children: {
+            control: 'text',
+            description: 'Content to be rendered inside the container.',
+            defaultValue: 'Dynamic Content',
+            table: {
+                type: { summary: 'ReactNode' },
+                defaultValue: { summary: 'Dynamic Content' },
+            },
+        },
+        active: {
+            control: 'boolean',
+            description: 'Determines if the component is visible.',
+            defaultValue: false,
+            table: {
+                type: { summary: 'boolean' },
+            },
+        },
+        onClick: {
+            action: 'clicked',
+            description: 'Optional click handler for the component.',
+            table: {
+                type: { summary: '() => void' },
+            },
+        },
+        style: {
+            control: 'object',
+            description: 'Optional styles to be applied to the component.',
+            table: {
+                type: { summary: 'CSSProperties' },
+            },
+        },
+    },
+    args: {
+        onClick: fn(),
+    },// 
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const DynamicContainer_Playground: Story = {
+    args: {
+        active: true
+    },
+    render: ({ ...args }) => {
+        return <DynamicContainer style={{ maxWidth: '500px' }} {...args} >
+            <DropDownMenu component_style="System-Style">
+                <DropDownMenuItem component_style='System-Style' component_label={'Option Example'} element_name={'example'} />
+                <DropDownMenuItem component_style='System-Style' component_label={'Option Example 2'} element_name={'example2'} />
+            </DropDownMenu>
+        </DynamicContainer>;
+    },
+};
+
+
+
