d05f97db65f2badac75a85efbff92a998c3bcce0
Sajjad-iq <rasooool59@gmail.com>
2024-08-06 11:11:09 +0300

    modify Table story

--- src/stories/components/Tables/Table/Table.stories.tsx
+++ src/stories/components/Tables/Table/Table.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { ComponentProps } from 'react';
+import { ComponentProps, useState } from 'react';
 import { Table, TableCell, TableCellContainer, TableHeaderCell } from '@/index'
 
 type StoryProps = ComponentProps<typeof Table>
@@ -31,6 +31,64 @@ export const Tables_Playground: Story = {
         </TableCellContainer>
     },
     render: ({ ...args }) => {
-        return <Table {...args} />;
+
+        const [data, setData] = useState([
+            {
+                label: 'User Name',
+                id: '1',
+                cells: ['John Doe', 'Jane Smith'],
+            },
+            {
+                label: 'User Email',
+                id: '2',
+                cells: ['jane.smith@example.com', 'john.doe@example.com',],
+            },
+            {
+                label: 'User Phone Number',
+                id: '3',
+                cells: ['555-1234', '555-5678'],
+            }
+        ]);
+
+        const renderTableHeaderCells = () => {
+            return data.map((item) => (
+                <TableHeaderCell
+                    key={item.id}
+                    resizable={true}
+                    component_size="M"
+                    label={item.label}
+                    id={item.id}
+                />
+            ));
+        };
+
+        const renderTableBodyCells = () => {
+            const numRows = Math.max(...data.map(item => item.cells.length));
+            const rows = [];
+
+            for (let i = 0; i < numRows; i++) {
+                rows.push(
+                    <TableCellContainer
+                        key={i}
+                        isChecked={false}
+                    >
+                        {data.map(item => (
+                            <TableCell
+                                key={`${item.id}-${i}`}
+                                cellLabel={item.cells[i] || ''}
+                                component_size="M"
+                            />
+                        ))}
+                    </TableCellContainer>
+                );
+            }
+
+            return rows;
+        };
+        return (<Table
+            table_header_cells_children_data={data}
+            table_header_cells_children_setter_data={setData}
+            table_header_cells_children={renderTableHeaderCells()}
+            table_body_cells_children={renderTableBodyCells()} />);
     },
 };
