ae07887be252f51e0978a308d462c4228b1f4d5b
Haider Daij <haiderduaij@gmail.com>
2024-08-29 14:48:24 +0300

    review

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,8 @@
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-import { afterEach } from 'node:test'
-
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { afterEach } from "node:test";
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
-    cleanup();
-})
+  cleanup();
+});
