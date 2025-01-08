2754cdaf8499524a1ad7b4afdf29241299d04a6b
Sajjad-iq <rasooool59@gmail.com>
2025-01-08 22:55:14 +0300

    try refactor the tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import Tooltip from "./lib/components/base/tooltips/tooltip_v2";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import Tooltip, { ContentSide, TooltipType } from "./lib/components/base/tooltips/tooltip_v2";
 
 function App() {
   return (
 
-    <div>
-      <Tooltip />
-    </div>
+    <TooltipProvider>
+      <Tooltip open text="hello" disabled={true} contentSide={ContentSide.LEFT} type={TooltipType.PRIMARY} size="small" tip={true}>
+        <button>open</button>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
 
--- src/lib/components/base/tooltips/tooltip_v2/index.tsx
+++ src/lib/components/base/tooltips/tooltip_v2/index.tsx
@@ -1,52 +1,46 @@
 import { cva } from "class-variance-authority";
 import { ReactNode } from "react";
-import { twMerge } from 'tailwind-merge'
-
-
+import { twMerge } from "tailwind-merge";
+import * as RadixTooltip from "@radix-ui/react-tooltip";
+import '../../../../styles/typography copy/index.scss';
 export enum TooltipType {
-    SUCCESS = 'success',
-    ERROR = 'error',
-    WARNING = 'warning',
-    SECONDARY = 'secondary',
+    PRIMARY = "primary",
+    SECONDARY = "secondary",
 }
 
 export enum ContentSide {
-    TOP = 'top',
-    RIGHT = 'right',
-    BOTTOM = 'bottom',
-    LEFT = 'left',
+    TOP = "top",
+    RIGHT = "right",
+    BOTTOM = "bottom",
+    LEFT = "left",
 }
 
 export enum ContentAlign {
-    START = 'start',
-    CENTER = 'center',
-    END = 'end',
+    START = "start",
+    CENTER = "center",
+    END = "end",
 }
 
-
-const tooltip = cva(["font-semibold", "border", "rounded"], {
+const tooltipStyles = cva("Body-typography-Medium-Regular rounded-[4px] px-2", {
     variants: {
         intent: {
-            primary: ["bg-blue-500", "text-white", "border-transparent"],
-            // **or**
-            // primary: "bg-blue-500 text-white border-transparent hover:bg-blue-600",
-            secondary: ["bg-white", "text-gray-800", "border-gray-400"],
+            primary: "bg-[--black-600] text-[--content-system-global-primary] border-transparent",
+            secondary: "bg-white text-gray-800 border-gray-400",
         },
         size: {
-            small: ["text-sm", "py-1", "px-2"],
-            medium: ["text-base", "py-2", "px-4"],
+            small: "",
+            medium: "",
         },
-        // `boolean` variants are also supported!
         disabled: {
             false: null,
-            true: ["opacity-50", "cursor-not-allowed"],
+            true: "opacity-50 cursor-not-allowed",
         },
     },
     compoundVariants: [
         {
             intent: "primary",
             disabled: false,
-            class: "hover:bg-blue-600",
+            class: "hover:bg-[--black-700]"
         },
         {
             intent: "secondary",
@@ -56,8 +50,6 @@ const tooltip = cva(["font-semibold", "border", "rounded"], {
         {
             intent: "primary",
             size: "medium",
-            // **or** if you're a React.js user, `className` may feel more consistent:
-            // className: "uppercase"
             class: "uppercase",
         },
     ],
@@ -68,10 +60,8 @@ const tooltip = cva(["font-semibold", "border", "rounded"], {
     },
 });
 
-
-interface Props {
-    text: ReactNode;
-    children: ReactNode | ReactNode[];
+interface TooltipProps extends React.HTMLAttributes<HTMLSpanElement> {
+    children: ReactNode;
     onOpenChange?: (open: boolean) => void;
     open?: boolean;
     contentSide?: ContentSide;
@@ -79,32 +69,49 @@ interface Props {
     avoidCollisions?: boolean;
     tip?: boolean;
     delay?: number;
-    invertColors?: boolean;
-    centerText?: boolean;
     type?: TooltipType;
+    size?: "small" | "medium";
+    disabled?: boolean;
+    text:ReactNode
 }
 
-const Tooltip: React.FC<any> = ({
-    text,
+const Tooltip: React.FC<TooltipProps> = ({
     children,
-    onOpenChange,
     open,
-    type,
-    contentSide = ContentSide.BOTTOM,
+    text,
+    onOpenChange,
+    type = TooltipType.PRIMARY,
+    contentSide = ContentSide.TOP,
     contentAlign = ContentAlign.CENTER,
     avoidCollisions = true,
-    delay = 400,
+    delay = 100,
     tip = true,
-    invertColors = true,
-    centerText = true,
+    size = "medium",
+    ...props
 }) => {
-
-
     return (
-        <span className={twMerge(tooltip({ intent: "primary", size: "medium", disabled: false }))} >
-            {children}
-        </span>
-    )
+        <RadixTooltip.Root
+        delayDuration={delay}
+        {...(typeof open !== 'undefined' && { open })}
+        {...(onOpenChange && { onOpenChange })}
+         >
+            <RadixTooltip.Trigger aria-label="Open tooltip" asChild>
+                <span className="text-blue-500 text-md">{children}</span>
+            </RadixTooltip.Trigger>
+
+            <RadixTooltip.Content
+                sideOffset={10}
+                side={contentSide}
+                align={contentAlign}
+                avoidCollisions={avoidCollisions}
+                className={twMerge(tooltipStyles({ intent: type, size }))}
+                {...props}
+            >
+                {text}
+                {tip && <RadixTooltip.Arrow />}
+            </RadixTooltip.Content>
+        </RadixTooltip.Root>
+    );
 };
 
 export default Tooltip;
