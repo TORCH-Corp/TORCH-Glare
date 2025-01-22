7b5f154aabaf28819d6213303ae26ca172e39f0c
Sajjad-iq <rasooool59@gmail.com>
2025-01-22 20:06:43 +0300

    clean up

--- src/lib/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { convertImageFileToDataUrl } from "./convertImageFileToDataUrl";
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs))
-}
-
-export { convertImageFileToDataUrl, cn }
