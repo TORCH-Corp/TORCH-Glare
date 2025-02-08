31cc04cb01a3454d5bf1e66506a0ce601be03988
Sajjad-iq <rasooool59@gmail.com>
2025-02-08 04:19:21 -0500

    Add horizontal padding to XL variant text in DropDownButton
    - Added px-[4px] to text element in XL variant
    - Ensures consistent text spacing in dropdown button

--- src/lib/components/base/DropDownButton.tsx
+++ src/lib/components/base/DropDownButton.tsx
@@ -193,7 +193,7 @@ export const dropdownButtonStyles = cva(
           "[&_span]:h-[28px] [&_span]:w-[28px] [&_p]:typography-body-large-medium",
         ],
         XL: [
-          "h-[40px] p-[4px] rounded-[6px] [&_span]:h-[32px] [&_span]:w-[32px] [&_p]:typography-body-small-regular",
+          "h-[40px] p-[4px] rounded-[6px] [&_span]:h-[32px] [&_span]:w-[32px] [&_p]:typography-body-small-regular [&_p]:px-[4px]",
         ],
       },
     },
