1c098721286ffe13ec8e1e99f34ef4d4679e41f0
Sajjad-iq <rasooool59@gmail.com>
2025-02-10 01:37:00 -0500

    Update package version and add Radix UI dependencies
    - Bumped package version to 0.0.6
    - Added new Radix UI dependencies: dropdown-menu, popover, select, and slot
    - Refactored AttachedPic component import paths in lib and torch directories

--- src/lib/components/base/AttachedPic.tsx
+++ src/lib/components/base/AttachedPic.tsx
@@ -1,8 +1,6 @@
 import { HTMLAttributes, ReactNode } from "react";
-import { Button } from "../Button";
-import { cn } from "../utils";
-
-
+import { Button } from "./Button";
+import { cn } from "./utils";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   src: any;
@@ -10,19 +8,19 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   onHide?: () => void;
 }
 
-export function AttachedPic({
-  src,
-  header,
-  onHide,
-  ...props
-}: Props) {
+export function AttachedPic({ src, header, onHide, ...props }: Props) {
   return (
     <section
       {...props}
-      className={cn(' overflow-hidden flex flex-col items-center justify-center w-80 p-2 gap-2 rounded-md border shadow-md border-[var(--border-presentation-global-primary)] bg-[var(--background-presentation-form-base)]', props.className)}
+      className={cn(
+        " overflow-hidden flex flex-col items-center justify-center w-80 p-2 gap-2 rounded-md border shadow-md border-[var(--border-presentation-global-primary)] bg-[var(--background-presentation-form-base)]",
+        props.className
+      )}
     >
       <section className="flex items-center justify-between w-full m-0">
-        <p className="m-0 text-[var(--content-presentation-global-primary)] typography-display-medium-semibold">{header}</p>
+        <p className="m-0 text-[var(--content-presentation-global-primary)] typography-display-medium-semibold">
+          {header}
+        </p>
         <Button onClick={onHide} size="M" buttonType="icon">
           <i className="ri-close-line text-[16px]"></i>
         </Button>
