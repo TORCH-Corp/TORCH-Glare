50454fb52ea46c46bf2c4d31d0476c17946b00ed
Sajjad-iq <rasooool59@gmail.com>
2025-02-27 11:57:01 +0300

    migrate to tailwind v4 and react 19

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   resolve: {
@@ -6,5 +8,9 @@ export default defineConfig({
       "@": "/lib", // Simple alias for the "src" directory
     },
   },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 });
 
