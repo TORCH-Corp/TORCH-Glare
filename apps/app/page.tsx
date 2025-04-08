d40465b879ece08aac18448fd585c270098fb8b3
Sajjad-iq <rasooool59@gmail.com>
2025-04-08 13:36:04 +0300

    Refactor page component: Replace ScrollArea with a Toggle for improved UI interaction. Update Button component to include loading state and enhance styling options with new props. Remove deprecated Toggle-dev component to clean up the codebase.

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -4,29 +4,19 @@ import { InputOTP } from "@/components/InputOTP";
 import { InputOTPSlot } from "@/components/InputOTP";
 import { InputOTPGroup } from "@/components/InputOTP";
 import Examples from "../exmples";
-import { ScrollArea } from "@/components/ScrollArea";
-import { Divider } from "@/components/Divider";
+import { Toggle } from "@/components/Toggle";
 
 export default function page() {
 
-  const tags = Array.from({ length: 50 }).map(
-    (_, i, a) => `v1.2.0-beta.${a.length - i}`
-  )
 
   return (
-    <ScrollArea className="h-72 w-48 rounded-md border">
-      <div className="p-4">
-        <h4 className="mb-4 text-sm font-medium leading-none text-content-presentation-global-primary">Tags</h4>
-        {tags.map((tag) => (
-          <>
-            <div key={tag} className="text-sm text-content-presentation-global-primary">
-              {tag}
-            </div>
-            <Divider className="my-2" />
-          </>
-        ))}
-      </div>
-    </ScrollArea>
+    <div className="flex flex-col gap-4 bg-black w-full h-full">
+      <Toggle value={"hlkklh"} >
+        B
+      </Toggle>
+    </div>
+
+
   );
 }
 
--- apps/lib/components/Button.tsx
+++ apps/lib/components/Button.tsx
@@ -4,7 +4,119 @@ import { Slot } from "@radix-ui/react-slot";
 import { cn } from "../utils/cn";
 import { ButtonVariant, Themes } from "../utils/types";
 
-export const buttonVariants = cva(
+interface Props
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+  VariantProps<typeof buttonVariants> {
+  is_loading?: boolean;
+  disabled?: boolean;
+  asChild?: boolean;
+  variant?: ButtonVariant;
+  as?: React.ElementType;
+  theme?: Themes
+  containerClassName?: string
+}
+export const Button = forwardRef<HTMLButtonElement, Props>(
+  (
+    {
+      is_loading = false,
+      variant,
+      size,
+      asChild,
+      as: Tag = "button",
+      buttonType,
+      className,
+      disabled,
+      theme,
+      children,
+      containerClassName,
+      ...props
+    },
+    ref
+  ) => {
+    const Component = asChild ? Slot : Tag;
+
+    return (
+      <Component
+        {...props}
+        data-theme={theme}
+        disabled={disabled || is_loading}
+        ref={ref}
+        className={cn(
+          buttonVariants({
+            variant,
+            size,
+            is_loading,
+            buttonType,
+            disabled,
+          }),
+          className
+        )}
+      >
+        {asChild ? (
+          React.cloneElement(
+            children as React.ReactElement,
+            {},
+            <div className={cn("px-[3px] flex items-center justify-center gap-[3px] has-[>i]:p-0", containerClassName)}>
+              {(children as React.ReactElement<any>).props.children}
+              {is_loading && <LoadingIcon size={size} />}
+            </div>
+          )
+        ) : (
+          <div className={cn("px-[3px] flex items-center justify-center gap-[3px] has-[>i]:p-0", containerClassName)}>
+            {children}
+            {is_loading && <LoadingIcon size={size} />}
+          </div>
+        )}
+      </Component>
+    );
+  }
+);
+
+Button.displayName = "Button"
+
+
+
+export function LoadingIcon({ size, className }: {
+  size?: "S" | "M" | "L" | "XL" | null;
+  className?: string;
+}) {
+  const iconVariants = cva("animate-spin ", {
+    variants: {
+      size: {
+        S: "w-[12px] h-[12px]",
+        M: "w-[18px] h-[18px]",
+        L: "w-[20px] h-[20px]",
+        XL: "w-[20px] h-[20px]"
+      },
+    },
+    defaultVariants: {
+      size: "M",
+    },
+  });
+
+  return (
+    <svg
+      className={cn(iconVariants({ size, className }))}
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6ZM2.25 6C2.25 8.07107 3.92893 9.75 6 9.75C8.07107 9.75 9.75 8.07107 9.75 6C9.75 3.92893 8.07107 2.25 6 2.25C3.92893 2.25 2.25 3.92893 2.25 6Z"
+        fill="#F4F4F4"
+      />
+      <path
+        d="M11 6C11 5.34339 10.8707 4.69321 10.6194 4.08658C10.3681 3.47995 9.99983 2.92876 9.53553 2.46447C9.07124 2.00017 8.52004 1.63188 7.91342 1.3806C7.30679 1.12933 6.65661 1 6 1V2.25C6.49246 2.25 6.98009 2.347 7.43506 2.53545C7.89003 2.72391 8.30343 3.00013 8.65165 3.34835C8.99987 3.69657 9.27609 4.10997 9.46455 4.56494C9.653 5.01991 9.75 5.50754 9.75 6H11Z"
+        fill="#0075FF"
+      />
+    </svg>
+  );
+}
+
+
+
+
+const buttonVariants = cva(
   "flex items-center whitespace-nowrap justify-center  transition-[background,color] duration-200 ease-in-out border border-transparent outline-none leading-none [&-i]:!leading-none",
   {
     variants: {
@@ -149,112 +261,3 @@ export const buttonVariants = cva(
     ],
   }
 );
-
-interface Props
-  extends ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
-  is_loading?: boolean;
-  disabled?: boolean;
-  asChild?: boolean;
-  variant?: ButtonVariant;
-  as?: React.ElementType;
-  theme?: Themes
-  containerClassName?: string
-}
-export const Button = forwardRef<HTMLButtonElement, Props>(
-  (
-    {
-      is_loading = false,
-      variant,
-      size,
-      asChild,
-      as: Tag = "button",
-      buttonType,
-      className,
-      disabled,
-      theme,
-      children,
-      containerClassName,
-      ...props
-    },
-    ref
-  ) => {
-    const Component = asChild ? Slot : Tag;
-
-    return (
-      <Component
-        {...props}
-        data-theme={theme}
-        disabled={disabled || is_loading}
-        ref={ref}
-        className={cn(
-          buttonVariants({
-            variant,
-            size,
-            is_loading,
-            buttonType,
-            disabled,
-          }),
-          className
-        )}
-      >
-        {asChild ? (
-          React.cloneElement(
-            children as React.ReactElement,
-            {},
-            <div className={cn("px-[3px] flex items-center justify-center gap-[3px] has-[>i]:p-0", containerClassName)}>
-              {(children as React.ReactElement<any>).props.children}
-              {is_loading && <LoadingIcon size={size} />}
-            </div>
-          )
-        ) : (
-          <div className={cn("px-[3px] flex items-center justify-center gap-[3px] has-[>i]:p-0", containerClassName)}>
-            {children}
-            {is_loading && <LoadingIcon size={size} />}
-          </div>
-        )}
-      </Component>
-    );
-  }
-);
-
-Button.displayName = "Button"
-
-
-
-export function LoadingIcon({ size, className }: {
-  size?: "S" | "M" | "L" | "XL" | null;
-  className?: string;
-}) {
-  const iconVariants = cva("animate-spin ", {
-    variants: {
-      size: {
-        S: "w-[12px] h-[12px]",
-        M: "w-[18px] h-[18px]",
-        L: "w-[20px] h-[20px]",
-        XL: "w-[20px] h-[20px]"
-      },
-    },
-    defaultVariants: {
-      size: "M",
-    },
-  });
-
-  return (
-    <svg
-      className={cn(iconVariants({ size, className }))}
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6ZM2.25 6C2.25 8.07107 3.92893 9.75 6 9.75C8.07107 9.75 9.75 8.07107 9.75 6C9.75 3.92893 8.07107 2.25 6 2.25C3.92893 2.25 2.25 3.92893 2.25 6Z"
-        fill="#F4F4F4"
-      />
-      <path
-        d="M11 6C11 5.34339 10.8707 4.69321 10.6194 4.08658C10.3681 3.47995 9.99983 2.92876 9.53553 2.46447C9.07124 2.00017 8.52004 1.63188 7.91342 1.3806C7.30679 1.12933 6.65661 1 6 1V2.25C6.49246 2.25 6.98009 2.347 7.43506 2.53545C7.89003 2.72391 8.30343 3.00013 8.65165 3.34835C8.99987 3.69657 9.27609 4.10997 9.46455 4.56494C9.653 5.01991 9.75 5.50754 9.75 6H11Z"
-        fill="#0075FF"
-      />
-    </svg>
-  );
-}
