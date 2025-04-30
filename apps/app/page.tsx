897d21251221d2271669ca178a33cc859c2aa3fa
Sajjad-iq <rasooool59@gmail.com>
2025-04-30 11:57:50 +0300

    Enhance BarChart implementation in page.tsx: Introduce new chart data structure and configuration, update chart rendering with desktop and mobile data, and add chart legend and labels for improved data visualization. Update globals.css with new color variables for chart themes.

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,64 +1,47 @@
 'use client'
-import { Bar, BarChart } from 'recharts';
-import { ChartTooltip } from '@/components/Charts';
+import { Bar, BarChart, LabelList, XAxis } from 'recharts';
+import { ChartConfig, ChartLegend, ChartLegendContent, ChartTooltip } from '@/components/Charts';
 import { ChartTooltipContent } from '@/components/Charts';
 import { ChartContainer } from '@/components/Charts';
 
-// Also add the missing data
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 export default function page() {
 
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ]
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig
 
   return (
-    <div className='w-full h-full bg-background-system-body-primary'>
-      <ChartContainer>
-        <BarChart data={data}>
-          <Bar dataKey="value" />
+    <div className='w-full h-full'>
+      <ChartContainer config={chartConfig}>
+        <BarChart data={chartData}>
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value: string) => value.slice(0, 3)}
+          />
           <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <LabelList className="fill-[--color-desktop]" />
         </BarChart>
       </ChartContainer>
     </div>
