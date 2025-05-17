b0a4a827f28a455c3d88814ce31bf03de1b631d3
Sajjad-iq <rasooool59@gmail.com>
2025-05-17 11:37:50 +0300

    Add react-hot-toast for notification support in page.tsx

--- apps/app/page.tsx
+++ apps/app/page.tsx
@@ -1,71 +1,41 @@
 'use client'
-import { Button } from '@/components/Button';
-import { Controller, useForm } from 'react-hook-form';
-import { BadgeField } from '@/components/BadgeField';
-import { Tag } from '@/hooks/useTagSelection';
-import { DatePicker } from '@/components/DatePicker';
-import { SlideDatePicker } from '@/components/SlideDatePicker';
-import { InputField } from '@/components/InputField';
-import { PopoverItem } from '@/components/Popover';
+import toast, { Toaster } from 'react-hot-toast';
+
+const notify = () => toast('Here is your toast.');
+
 export default function Page() {
-  const { control, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  }
-  const tags: Tag[] = [
-    { id: '1', name: 'Electronics', isSelected: false, variant: 'blue' },
-    { id: '2', name: 'Books', isSelected: false, variant: 'green' },
-    { id: '3', name: 'Clothing', isSelected: false, variant: 'purple' },
-    { id: '4', name: 'Home', isSelected: false, variant: 'yellow' },
-    { id: '5', name: 'Sports', isSelected: false, variant: 'navy' },
-    { id: '8', name: 'Limited Edition', isSelected: false, variant: 'cocktailGreen' },
-  ]
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-6 w-full p-5'>
-      <Controller
-        name="slideDatePicker"
-        control={control}
-        render={({ field }) => (
-          <DatePicker
-            {...field}
-          />
-        )}
-      />
+    <div>
+      <button onClick={notify}>Make me a toast</button>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{
+        }}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          removeDelay: 1000,
+          style: {
+            background: 'var(--content-background-body-base)',
+            color: 'var(--content-presentation-global-primary)',
+          },
 
-      <Controller
-        name="slideDatePicker"
-        control={control}
-        render={({ field }) => (
-          <SlideDatePicker
-            {...field}
-          />
-        )}
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            iconTheme: {
+              primary: 'var(--content-presentation-global-primary)',
+              secondary: 'var(--content-presentation-global-secondary)',
+            },
+          },
+        }}
       />
-
-      <Controller
-        name="slideDatePicker"
-        control={control}
-        render={({ field }) => (
-          <InputField
-            {...field}
-            popoverChildren={
-              tags.map((tag) => (
-                <PopoverItem
-                  key={tag.id}
-                  onClick={() => {
-                    console.log(tag);
-                  }}
-                >
-                  {tag.name}
-                </PopoverItem>
-              ))
-            }
-          />
-        )}
-      />
-
-      <Button theme='light' type='submit'>Submit</Button>
-    </form>
+    </div>
   );
 }
 
